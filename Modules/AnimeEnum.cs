85ed25a5144f9ea520e194c9c3165538b10c8672
Иван Адерейко <vanja.1996@outlook.com>
2020-04-26 16:51:41 +0300

    Add project files.

---
+++ Modules/AnimeEnum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace DiscordBot.Modules
+{
+
+    public class AnimeEnum
+    {
+       public static int Getrand()
+        {
+            Random random = new Random();
+
+            int Randchislo = random.Next(1, 10);
+
+            return Randchislo;
+
+        }
+      
+
+        public void Vibor()
+        {
+        //TODO: здесь требуется сделать рандомный вывод 
+        switch (Randchislo)
+
+
+
+        }
+    }
+}
---
+++ Modules/Commands.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBot.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("Anime")]
+        public async Task Ping()
+        {
+            //TODO: сделать вывод рандомного значения
+            await ReplyAsync("P ong");
+        }
+    }
+}
